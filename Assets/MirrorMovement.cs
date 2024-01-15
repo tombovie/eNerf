435c0fa2d58d2f2b9b594dbb5d2664e7e2cba9b3
BramBeysens <80328237+BramBeysens@users.noreply.github.com>
2024-01-15 15:47:02 +0100

    Initial commit 3.0

---
+++ Assets/MirrorMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorMovement : MonoBehaviour
+{
+    public Transform playerTarget;
+    public Transform mirror;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get position of camera
+        Vector3 localPlayer = mirror.InverseTransformPoint(playerTarget.position);
+        //invert position of player along z-axis
+        transform.position = mirror.TransformPoint(new Vector3(localPlayer.x, localPlayer.y, -localPlayer.z));
+
+        //let camera look at x inverted position
+        Vector3 lookAtMirror = mirror.TransformPoint(new Vector3(-localPlayer.x, localPlayer.y, localPlayer.z));
+        transform.LookAt(lookAtMirror);
+        
+
+    }
+}
