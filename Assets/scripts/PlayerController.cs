e317f1bfed441aef502f739bf66e43209884af23
tombovie <Tom.bovie@gmail.com>
2024-01-24 14:12:54 +0100

    character controller
    Now the character can move, jump and dance when you hold down P

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+
+    private CharacterController characterController;
+    private Animator animator;
+
+    [SerializeField]
+    private float movementSpeed, rotationSpeed, jumpSpeed, gravity;
+
+    private Vector3 movementDirection = Vector3.zero;
+    private bool playerGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerGrounded = characterController.isGrounded;
+
+        //movement
+        Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
+        characterController.Move(inputMovement * Time.deltaTime);
+
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
+
+
+        //jumping
+        if (Input.GetButton("Jump") && playerGrounded)
+        {
+            movementDirection.y = jumpSpeed;
+        }
+        movementDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(movementDirection * Time.deltaTime);
+
+        // Apply dancing when the "A" key is pressed
+        if (Input.GetKey(KeyCode.P))
+        {
+            animator.SetTrigger("isDancing"); // Trigger the "Dance" animation
+        }
+        else
+        {
+            animator.ResetTrigger("isDancing");
+        }
+
+        //animations
+        animator.SetBool("isWalking", Input.GetAxisRaw("Vertical") != 0);
+        animator.SetBool("isJumping", !characterController.isGrounded);
+        Debug.Log("" + characterController.isGrounded);
+    }
+}
