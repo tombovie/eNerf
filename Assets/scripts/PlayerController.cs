51aa6c8f811e662de1aa9a846c3d68791ed2cfbd
tombovie <Tom.bovie@gmail.com>
2024-03-19 20:16:43 +0100

    vr walking joystick

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.InputSystem.XR;
 
 [RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class PlayerController : MonoBehaviour
@@ -20,11 +23,14 @@ public class PlayerController : MonoBehaviour
     private int count = 0;
     private bool movingBackwards = false;
 
+    private XRInputSubsystem inputSystem;  // New variable
+
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        //inputSystem = XRInputSystem.GetInputSystem();  // Get XR Input System
     }
 
     // Update is called once per frame
