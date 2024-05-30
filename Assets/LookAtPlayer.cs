50974ed1d73cfdbc851e19df74b7273ae49120dc
tombovie <Tom.bovie@gmail.com>
2024-05-30 13:21:18 +0200

    lookatplayer

--- Assets/LookAtPlayer.cs
+++ Assets/LookAtPlayer.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-
     public Transform player;
 
 
-    // Update is called once per frame
     void Update()
     {
         transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
